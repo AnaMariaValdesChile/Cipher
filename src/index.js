73580c22ed59e569a174be6c4a18c4071538cf7f
Your Name <you@example.com>
2023-09-03 18:42:30 -0300

    html y css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
+
+const textArea = document.getElementById("textArea");
+const divText = document.getElementById("sale");
+
+textArea.addEventListener("keyup", () => {
+  divText.textContent = textArea.value;
+});
 
 console.log(cipher);
